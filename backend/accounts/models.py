3877a000e713fc9dea4d30b351ffffa500acfa0f
bocahrokok <indieunistudy@gmail.com>
2020-05-31 14:06:25 +0700

    custom model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,20 +4,21 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if(not email):
+    def create_user(self, email, name, major, password=None):
+        if not email:
             raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
+        user = self.model(email=email, name=name, major=major)
+        
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, name, password):
         user = self.create_user(email, name, password)
+
         user.is_admin = True
         user.is_superuser = True
         user.save()
@@ -26,18 +27,32 @@ class UserAccountManager(BaseUserManager):
 
 # Create your models here.
 class UserAccount(AbstractBaseUser, PermissionsMixin):
+    SOCIAL_CHOICE = (('instagram', 'instagram'),
+                     ('linkedin', 'linkedin'), ('resume', 'resume'))
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+
+    major = models.CharField(max_length=50)
+    # social_media = models.CharField( max_length=50, choices =  SOCIAL_CHOICE)
+
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
+
     object = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name', 'major']
 
     def get_full_name(self):
         return self.name
 
     def get_short_name(self):
         return self.name
+
+    def get_major(self):
+      return self.major
 
     def __str__(self):
         return self.email
